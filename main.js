8881edb2893491c3b629b5d08e9f3a4dd12f1d8c
haakensonb <untamedhawk44@gmail.com>
2016-10-20 22:52:55 -0400

    Added closing button

--- main.js
+++ main.js
@@ -6,17 +6,22 @@ var todoApp = {
     this.showTasks();
   },
   removeTask: function(item) {
-    var index = this.tasks.indexOf(item);
-    this.tasks.splice(index, 1);
+    for (var i = 0; i < this.tasks.length; i++) {
+      if(this.tasks[i].name == item){
+        this.tasks.splice(i, 1);
+      }
+    }
+
     this.showTasks();
   },
   showTasks: function() {
     var taskListEl = document.querySelector('.task-list');
     taskListEl.innerHTML = "";
     for (var i = 0; i < this.tasks.length; i++){
-      taskListEl.insertAdjacentHTML('beforeend', '<li class="task">' + this.tasks[i].name + '</li>');
+      taskListEl.insertAdjacentHTML('beforeend', '<div  class="task">' + '<li>' + this.tasks[i].name + '</li>' + '<div class="close hidden">X</div>' + '</div>');
     }
     //add click events after li are shown
+    this.addDeleteEvents();
     this.addClickEvents();
   },
   toggleCompleted: function (obj) {
@@ -25,22 +30,38 @@ var todoApp = {
     } else if (this.tasks[obj].completed === true) {
       this.tasks[obj].completed = false;
     }
+    //toggle css class to show change
     event.target.classList.toggle('complete');
   },
   addClickEvents: function () {
     var taskEl = document.querySelectorAll('.task');
     for (var i = 0; i < taskEl.length; i++){
-      taskEl[i].addEventListener('click', myEvent);
+      taskEl[i].addEventListener('click', myClickEvent);
     }
-    function myEvent () {
+    function myClickEvent () {
       //get text from li clicked
       var myTarget = event.target.innerHTML;
       //loop through tasks and if the name matches the li then toggleCompleted on that Task
       for (var i = 0; i < taskEl.length; i++){
-        if (todoApp.tasks[i].name == myTarget){
+        //dont check for target matching if task no longer exists
+        if (todoApp.tasks[i] !== undefined && todoApp.tasks[i].name == myTarget){
           todoApp.toggleCompleted(i);
         }
       }
+    }
+  },
+  addDeleteEvents: function () {
+    var closeEl = document.querySelectorAll('.close');
+    for (var i = 0; i < closeEl.length; i++){
+      closeEl[i].addEventListener('click', myDeleteEvent);
+    }
+
+    function myDeleteEvent () {
+      var target = event.target;
+      var parent = target.parentElement;
+      //access text of list item in front of .close div
+      var text = parent.firstChild.innerHTML;
+      todoApp.removeTask(text);
     }
   }
 };
@@ -61,6 +82,6 @@ document.addEventListener('DOMContentLoaded', function(){
       todoApp.addTask(newTaskEl.value);
       newTaskEl.value = '';
     }
-  })
+  });
 
 });
